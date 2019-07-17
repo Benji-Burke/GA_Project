dd03bcf166805285817cfffbcfd03cd28dc40c5f
Ben Burke <benjaminstevenburke@gmail.com>
2019-07-17 10:28:03 -0400

    getting this project going

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
 $(()=>{
-
-    const coin = 'BTC'
-    const endpoint = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,JPY,EUR,BTC?apikey=6ccfcc7c18aa8c9a843472090f99f174349d955a89cbfc6a69b0f78f3ace71c5`
-    
+    ///////////////////Price of the Coin//////////////////////
+    // const coin = 'BTC'
+    // const endpoint = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,JPY,EUR,BTC?apikey=6ccfcc7c18aa8c9a843472090f99f174349d955a89cbfc6a69b0f78f3ace71c5`
+    const $marketCapNumber = 100;
+    const endpoint=`https://min-api.cryptocompare.com/data/top/mktcapfull?limit=${$marketCapNumber}&tsym=USD&?apikey=6ccfcc7c18aa8c9a843472090f99f174349d955a89cbfc6a69b0f78f3ace71c5`
     console.log(endpoint)
-
+   
 const handleData = (data) =>{
-    console.log(data)
 
-    const $coindata = $('<h1>').text(data.USD)
-    $('.cointainer').append($coindata)
+//    for (let i =0; i<data.length; i++) {
+
+        // Name
+        let name = data.Data[0].CoinInfo.FullName;
+        //  price
+        let price = data.Data[0].DISPLAY.USD.PRICE;
+        //   marketcap
+        let $marcketCap = data.Data[0].DISPLAY.USD.MKTCAP;
+        // symbol e.g btc, xrp, ltc
+        let symbol = data.Data[0].DISPLAY.USD.FROMSYMBOL;
+        // img 
+        // let coinLogo = data.Data[0].DISPLAY.USD.IMAGEURL;
+    
+        
+        
+        
+        //   created inputs
+        const $coinprice = $('<h4>').text(`${price}`)
+        const $totalMarketCap = $('<h4>').text(`${$marcketCap}`)
+        const $ticker = $('<h4>').text(`${symbol}`)
+        const $nameOfCoin = $('<h3>').text(`${name}`)
+        // const $imgageOfCoin = $('<h2>').html(`<img: src =`$(coinLogo>`)
+        
+        const $tableRow = $('<tr>');
+        $tableRow.addClass('tableRow');
+        
+        const $tableData = $('<td>');
+        $tableData.addClass('tableData');
+        
+        let rowOfData = $tableRow.append('.tableData');
+        $('body').append(rowOfData);
+        
+
+
+
+
+
+
+    // append to the dom
+    $('.price').append($coinprice)
+    $('.marketCap').append($totalMarketCap)
+    $('.symbol').append($ticker)
+    $('.name').append($nameOfCoin)
+    // $('.logo').append(coinLogo)
+
+    
+    
 }
+
+
+
+
     $.ajax({
         url: endpoint
 
     }).then(handleData)
 
-});
+})
+
---
+++ app2.js
@@ -0,0 +1,20 @@
+// $(()=>{
+//     //////////////////////MKTCAP////////////////////////////////
+//         const $marketCap = 100;
+    
+//         const endpoint= `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=${$marketCap}&tsym=USD&?apikey=6ccfcc7c18aa8c9a843472090f99f174349d955a89cbfc6a69b0f78f3ace71c5`
+    
+//         console.log(endpoint);
+// const marketCap = (data) =>{
+//     // console.log(data.Data[0].RAW.USD.MKTCAP);
+   
+
+
+// }
+//  $.ajax({
+//      url: endpoint
+//  }).then(marketCap)
+
+//     })
+    
+   
